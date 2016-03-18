06040690145cd267e63b041e16254e747779a1bd
samzah <szahir@hotmail.com>
2016-03-18 17:07:06 -0400

    fixing indents

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-
 var $body = $('body');
 var $html = $('html');
 
 $html.on('keydown', function (e) {
-	var randTop = Math.random() * 1920;
-	var randLeft = Math.random() * 1920;
-	var $div = $('<div>');
-	$div.addClass('ball');
-	$body.append($div)
-	$div.css('top', randTop);
-	$div.css('left', randLeft);
+  var randTop = Math.random() * 1920;
+  var randLeft = Math.random() * 1920;
+  var $div = $('<div>');
+
+  $div.addClass('ball');
+  $body.append($div);
+  $div.css('top', randTop);
+  $div.css('left', randLeft);
 });
-
-
