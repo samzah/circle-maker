84871af2143ace04cf54ceb21e92316b03aae6bb
samzah <szahir@hotmail.com>
2016-03-18 17:05:57 -0400

    making circles

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+var $body = $('body');
+var $html = $('html');
+
+$html.on('keydown', function (e) {
+	var randTop = Math.random() * 1920;
+	var randLeft = Math.random() * 1920;
+	var $div = $('<div>');
+	$div.addClass('ball');
+	$body.append($div)
+	$div.css('top', randTop);
+	$div.css('left', randLeft);
+});
+
+
